f6878804ca081c710ed92bf8f855f3a6f0eb48bd
talmaashani2016 <talmaashani2016@my.fit.edu>
2026-01-31 15:35:46 -0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # 3D-Shirt
 # 3D-Shirt
+# OQBI-Celebration-Card
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,9 @@
-import CanvasModel from "./canvas";
-import Customizer from "./pages/Customizer";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <main className="app transition-all ease-in">
+    <main className="min-h-screen transition-all ease-in bg-[url('/bg-main.png')] bg-cover">
       <Home />
-      <CanvasModel />
-      <Customizer />
     </main>
   );
 }
